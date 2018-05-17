8af59b542dfe0756d7a99bb61667d616860e1822
Leandher Bessa de Almeida <leandher_bessa@hotmail.com>
2018-05-17 14:27:19 -0300

    Segurando sessão

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import { Navigation } from 'react-native-navigation';
 import Feed from './src/components/Feed';
 import Login from './src/screens/Login';
+import { AsyncStorage } from 'react-native';
 
 Navigation.registerComponent('Login', () => Login);
 Navigation.registerComponent('Feed', () => Feed);
 
-Navigation.startSingleScreenApp({
-    screen: {
-        screen: 'Login',
-        title: 'Login',
-    }
-});
+AsyncStorage.getItem('token')
+  .then(token => {
+    console.warn(token)
+    if(token) {
+      return {
+        screen: 'Feed',
+        title: 'Instalura'
+    };
+  }
+
+  return {
+    screen: 'Login',
+    title: 'Login',
+
+  }; 
+ })
+ .then(screen => Navigation.startSingleScreenApp({screen}));
